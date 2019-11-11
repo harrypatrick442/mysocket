cf5c4eb30d7e4454b56c3cc627b7f13206fa6c8b
harrypatrick442 <34253839+harrypatrick442@users.noreply.github.com>
2019-11-11 19:05:00 +0000

    implementing

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports={
+	Mysocket:require('./backend/Mysocket'),
+	MysocketChannelFactory:require('./backend/MysocketChannelFactory'),
+	MysocketCleanup:require('./backend/MysocketCleanup'),
+	MysocketEndpointLongpoll:require('./backend/MysocketEndpointLongpoll'),
+	MysocketEndpointWebsocket:require('./backend/MysocketEndpointWebsocket'),
+	Mysockets:require('./backend/Mysockets'),
+};
